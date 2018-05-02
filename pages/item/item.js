225d3099d1e7c397b2d398b0532d3a0251ca0c01
za-houpengle <za-houpengle@zaonline.com>
2018-05-02 19:23:14 +0800

    modify

--- pages/item/item.js
+++ pages/item/item.js
@@ -5,6 +5,9 @@ Page({
    * 页面的初始数据
    */
   data: {
+    item:{
+
+    }
   
   },
 
@@ -12,14 +15,40 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    console.log(options.id);
+    let item = {
+      "name": "臻爱父母基础套餐",
+      "price": 1600,
+      "image": "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2518268771.jpg",
+      "id": 1,
+      "projects": [
+        {
+          "name": "一般检查",
+          "description": "体重是否正常，有无体重不足、超重或肥胖；有无血压脉搏异常等"
+        },
+        {
+          "name": "总胆固醇（TC）",
+          "description": "筛查脂肪肝，胆管炎，胆囊炎，药物中毒性肝炎，酒精性肝炎和黄疸等。"
+        },
+        {
+          "name": "总胆固醇（TC）",
+          "description": "筛查脂肪肝，胆管炎，胆囊炎，药物中毒性肝炎，酒精性肝炎和黄疸等。"
+        },
+        {
+          "name": "总胆固醇（TC）",
+          "description": "筛查脂肪肝，胆管炎，胆囊炎，药物中毒性肝炎，酒精性肝炎和黄疸等。"
+        }
+      ]
+    };
+    let data = { "heads": ["项目名称", "项目介绍"], "list": item.projects, "columns": ["name", "description"]}
+    this.setData({item:item, tableData: data});
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+    console.log(this.data.item);
   },
 
   /**
